f10432272475d8d891785d3eab27d08acf8b27be
adadezer <adadezer@gmail.com>
2024-02-18 22:19:21 -0300

    feat: component Clue

---
+++ src/app/components/Clue.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface LocalClueProps {
+  clue: string;
+}
+
+function Clue({ clue }: LocalClueProps) {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex">
+        <h3>{clue}</h3>
+      </div>
+      <textarea
+        className="textarea textarea-bordered h-32 w-full bg-white bg-opacity-35 text-lg"
+        placeholder="Anote as pistas encontradas"
+      ></textarea>
+    </div>
+  );
+}
+
+export default Clue;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
 import Image from "next/image";
+import Clue from "./components/Clue";
 
 export default function Home() {
   return (
     <div className="container mx-auto flex flex-col p-2">
-      <div className="flex justify-center text-2xl font-semibold mb-3">
+      <div className="flex justify-center text-3xl font-semibold mb-5">
         Caderneta Sherlock Holmes
       </div>
       <label className="form-control">
-        <div className="text-lg mb-1">Anotações Gerais</div>
+        <div className="text-xl mb-1">Anotações Gerais</div>
         <textarea
-          className="textarea textarea-bordered h-24 bg-neutral-50 bg-opacity-30"
+          className="textarea textarea-bordered w-full h-32 bg-white bg-opacity-25 text-lg"
           placeholder="Escreva suas anotações gerais..."
         ></textarea>
       </label>
+
+      <div className="flex justify-center text-2xl font-semibold rounded-md bg-yellow-500 bg-opacity-35 border border-amber-950 mt-5 mb-3">
+        Pistas
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-4 text-lg">
+          <Clue clue="Farmácia" />
+          <Clue clue="Parque" />
+        </div>
+        <div className="flex gap-4 text-lg">
+          <Clue clue="Banco" />
+          <Clue clue="Teatro" />
+        </div>
+      </div>
     </div>
   );
 }
