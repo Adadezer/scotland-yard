1c6b21b50db79f6139de5b4ac13c98a16e3f5564
adadezer <adadezer@gmail.com>
2024-02-24 16:51:17 -0300

    feat: case selection

---
+++ src/app/cases/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import listCases from "@/helpers/listCases";
+
+function Cases() {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center gap-3 bg-gradient-to-br from-[#006fca] via-[#399ef1] to-[#006fca] text-white">
+      <div className="flex text-2xl mb-4 items-center font-semibold">
+        Escolha um caso para ser resolvido!
+      </div>
+      {listCases.map((cases) => (
+        <Link href={`/selectedCase/${cases.case}`} key={cases.case}>
+          <div className="flex items-center card min-w-60 shadow-xl bg-[#fcac00] bg-opacity-75 border border-amber-950">
+            <div className="card-body">
+              <div className="card-title">{cases.case}</div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Cases;
---
+++ src/helpers/listCases.tsx
@@ -0,0 +1,43 @@
+const listCases = [
+  {
+    case: "21",
+    title: "CASO 21: 'O CASO DA MORTE REESCRITA'",
+    description: `
+    O pano de boca do teatro se levanta para dar inicio ao terceiro ato de uma peça de mistério.
+    Sherlock Holmes e o doutor Watson assistem ao espetáculo.
+    O ator Roderik Garrick vestido com um berrante uniforme vermelho, deve resolver a enigmática morte de um oficial amigo.
+    
+    Nos bastidores o suplente Vance Miliyard observava; silenciosamente cada movimento do famoso ator Ogden Bennet.
+    O dono o teatro, aflito com a diminuição da venda de ingressos, caminha nervosamente por trás do cenário.
+    No palco a encantadora atriz Regina Fulton esposa do escritor da peca, Stephen Fulton, chora ao ser interrogada, enquanto Albert Boswell vestido com uma túnica verde para indicar que pertence à Brigada de Carabineiros, caminha em direção a ela.
+    
+    Garrick revela atordoado que Boswell é o assassino, Boswell, sentindo-se encurralado saca seu revólver, os dois homens lutam.
+    Um tiro ressoa no ar, depois outro, Boswell retrocede e cai sem vida sobre a ribalta.
+    O aplauso é breve. Regina Fulton grita histericamente pedindo um medico, Albert Boswell foi assassinado.
+    
+    Roderik Garrick jura que o segundo tiro não proveio da briga encenada, Milton Acker insiste em afirmar que carregou a arma com balas falsas, e o escritor da peça Stephen Fulton, desconsiderando os rumores sobre a existência de um caso amoroso entre a sua esposa e Boswell, jura que a única razão que o levou a reescrever a cena do confronto, substituindo a faca pelo revólver, foi o interesse de tornar a peça mais dramática.
+    `,
+    goal: `
+      Ao detetive cabe descobrir:
+      a ) quem matou o ator
+      b ) o motivo.
+    `,
+  },
+  {
+    case: "22",
+    title: "CASO 22: 'A MORTE DA BEZERRA'",
+    description: `
+    Betinho estava passeando no pasto junto com uns amigos, e viu um monte de Bezerros.
+    Um de seus amigos disse que odiava bezerros, e Betinho ficou triste.
+    Betinho disse que gostava, mas estava com fome e ficou imaginando se bezerro é gostoso.
+    Dias depois os amigos ficaram sabendo que um dos bezerros morreu.
+    `,
+    goal: `
+      Ao detetive cabe descobrir:
+      a ) Quem matou o bezerro
+      b ) O motivo.
+    `,
+  },
+];
+
+export default listCases;
